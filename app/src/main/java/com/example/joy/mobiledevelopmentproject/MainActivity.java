391190b1a59736ac7400af37f06ccaed646d699d
joy <joyc_y@hotmail.com>
2016-11-22 15:07:13 -0800

    ex of how to insert data

--- app/src/main/java/com/example/joy/mobiledevelopmentproject/MainActivity.java
+++ app/src/main/java/com/example/joy/mobiledevelopmentproject/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.joy.mobiledevelopmentproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*ArrayList<String> temp = new ArrayList<String>();
+        temp.add("1");
+        temp.add("tw");
+        temp.add("th");
+        temp.add("fo");
+
+
+        DatabaseHelper dbh;
+        dbh = new DatabaseHelper(this);
+        dbh.addRec("bills",temp);*/
     }
 }
